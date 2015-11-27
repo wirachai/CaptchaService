a5b50d17564d319bd01b5097d13b792bfeb2b7cf
wirachai <wirachai.me@gmail.com>
2015-11-27 10:54:14 +0700

    Add RandomNumber with test

---
+++ CaptchaService.Test/RandomNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace CaptchaService.Test
+{
+    [TestFixture]
+    internal class RandomNumberTest
+    {
+        [Test]
+        public void GetPattern_ShouldBeNumberInRange1To2()
+        {
+            RandomNumber random = new RandomNumber();
+            Assert.That(random.GetPattern(), Is.InRange(1, 2));
+        }
+
+        [Test]
+        public void GetOperand_ShouldBeNumberInRange1To9()
+        {
+            RandomNumber random = new RandomNumber();
+            Assert.That(random.GetOperand(), Is.InRange(1, 9));
+        }
+
+        [Test]
+        public void GetOperator_ShouldBeNumberInRange1To3()
+        {
+            RandomNumber random = new RandomNumber();
+            Assert.That(random.GetOperator(), Is.InRange(1, 3));
+        }
+    }
+}
---
+++ CaptchaService/RandomNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaptchaService
+{
+    public class RandomNumber
+    {
+        public int GetPattern()
+        {
+            return Random(1, 2);
+        }
+
+        public int GetOperand()
+        {
+            return Random(1, 9);
+        }
+
+        public int GetOperator()
+        {
+            return Random(1, 3);
+        }
+
+        private int Random(int min, int max)
+        {
+            return new Random().Next(min, max + 1);
+        }
+    }
+}
