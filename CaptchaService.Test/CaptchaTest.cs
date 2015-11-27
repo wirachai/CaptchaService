66fb3f4c5278566d0fb7bc8de71abd2db4a421ff
wirachai <wirachai.me@gmail.com>
2015-11-27 10:48:46 +0700

    Add Captcha with test

---
+++ CaptchaService.Test/CaptchaTest.cs
@@ -0,0 +1,151 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CaptchaService.Test
+{
+    [TestFixture]
+    internal class CaptchaTest
+    {
+        [Test]
+        public void LeftOperand_ShouldBe1_WhenInputIs1()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, 1);
+            Assert.AreEqual("1", captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void LeftOperand_ShouldBe2_WhenInputIs2()
+        {
+            Captcha captcha = new Captcha(2, 2, 1, 1);
+            Assert.AreEqual("2", captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void LeftOperand_ShouldBe9_WhenInputIs9()
+        {
+            Captcha captcha = new Captcha(2, 9, 1, 1);
+            Assert.AreEqual("9", captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void LeftOperand_ShouldThrowArgumentOutOfRangeException_WhenInputIsMinus1()
+        {
+            Captcha captcha = new Captcha(2, -1, 1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldBeONE_WhenInputIs1()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, 1);
+            Assert.AreEqual("ONE", captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldBeTWO_WhenInputIs2()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, 2);
+            Assert.AreEqual("TWO", captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldBeNINE_WhenInputIs9()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, 9);
+            Assert.AreEqual("NINE", captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldThrowArgumentOutOfRangeException_WhenInputIsMinus1()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, -1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldThrowArgumentOutOfRangeException_WhenInputIs10()
+        {
+            Captcha captcha = new Captcha(2, 1, 1, 10);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void Operator_ShouldBePlus_WhenInputIs1()
+        {
+            Captcha captcha = new Captcha(pattern: 2, leftOperand: 1, operatorValue: 1, rightOperand: 1);
+            Assert.AreEqual("+", captcha.GetOperator());
+        }
+
+        [Test]
+        public void Operator_ShouldBeMultiply_WhenInputIs2()
+        {
+            Captcha captcha = new Captcha(pattern: 2, leftOperand: 1, operatorValue: 2, rightOperand: 1);
+            Assert.AreEqual("*", captcha.GetOperator());
+        }
+
+        [Test]
+        public void Operator_ShouldBeMinus_WhenInputIs3()
+        {
+            Captcha captcha = new Captcha(pattern: 2, leftOperand: 1, operatorValue: 3, rightOperand: 1);
+            Assert.AreEqual("-", captcha.GetOperator());
+        }
+
+        [Test]
+        public void Operator_ShouldThrowArgumentOutOfRangeException_WhenInputNotIn1To3()
+        {
+            Captcha captcha = new Captcha(2, 1, 0, 10);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetOperator());
+            captcha = new Captcha(2, 1, 4, 10);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetOperator());
+        }
+
+        [Test]
+        public void LeftOperand_ShouldBeTWO_WhenPatternIs1AndLeftOperandIs2()
+        {
+            var captcha = new Captcha(1, 2, 1, 1);
+            Assert.AreEqual("TWO", captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void LeftOperand_ShouldBeONE_WhenPatternIs1AndLeftOperandIs1()
+        {
+            Captcha captcha = new Captcha(pattern: 1, leftOperand: 1, operatorValue: 2, rightOperand: 1);
+            Assert.AreEqual("ONE", captcha.GetLeftOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldBe1_WhenPatternIs1AndRightOperandIs1()
+        {
+            Captcha captcha = new Captcha(pattern: 1, leftOperand: 1, operatorValue: 2, rightOperand: 1);
+            Assert.AreEqual("1", captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void RightOperand_ShouldThrowArgumentOutOfRangeException_WhenPatternIs1AndRightOperandIs10()
+        {
+            Captcha captcha = new Captcha(1, 1, 1, 10);
+            Assert.Throws<ArgumentOutOfRangeException>(() => captcha.GetRightOperand());
+        }
+
+        [Test]
+        public void Get_ShouldBeOnePlus1_WhenPatternIs1_LeftOperandIs1_OperatorIs1_RightOperandIs1()
+        {
+            var captcha = new Captcha(1, 1, 1, 1);
+            Assert.AreEqual("ONE + 1", captcha.Get());
+        }
+
+        [Test]
+        public void Get_ShouldBe1MultiplyOne_WhenPatternIs2_LeftOperandIs1_OperatorIs2_RightOperandIs1()
+        {
+            var captcha = new Captcha(2, 1, 2, 1);
+            Assert.AreEqual("1 * ONE", captcha.Get());
+        }
+
+        [Test]
+        public void Get_ShouldBeTWOMultiply2_WhenPatternIs1_LeftOperandIs2_OperatorIs2_RightOperandIs2()
+        {
+            var captcha = new Captcha(1, 2, 2, 2);
+            Assert.AreEqual("TWO * 2", captcha.Get());
+        }
+    }
+}
---
+++ CaptchaService/Captcha.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaptchaService
+{
+    public class Captcha
+    {
+        private int leftOperand;
+        private int rightOperand;
+        private int operatorValue;
+        private Pattern pattern;
+
+        private enum Pattern
+        {
+            WORD_OPERATOR_NUMBER = 1
+        }
+
+        private List<string> operands = new List<string> { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE" };
+
+        public Captcha(int pattern, int leftOperand, int operatorValue, int rightOperand)
+        {
+            this.leftOperand = leftOperand;
+            this.rightOperand = rightOperand;
+            this.operatorValue = operatorValue;
+            this.pattern = (Pattern)pattern;
+        }
+
+        public string Get()
+        {
+            return string.Format("{0} {1} {2}", GetLeftOperand(), GetOperator(), GetRightOperand());
+        }
+
+        public string GetLeftOperand()
+        {
+            ValidateInRangeZeroToNine(leftOperand);
+            if (pattern == Pattern.WORD_OPERATOR_NUMBER)
+            {
+                return GetOperandWord(leftOperand);
+            }
+            return leftOperand.ToString();
+        }
+
+        public string GetRightOperand()
+        {
+            ValidateInRangeZeroToNine(rightOperand);
+            if (pattern == Pattern.WORD_OPERATOR_NUMBER)
+            {
+                return rightOperand.ToString();
+            }
+            return GetOperandWord(rightOperand);
+        }
+
+        private void ValidateInRangeZeroToNine(int operand)
+        {
+            if (IsOperandNotInRangeZeroToNine(operand))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private bool IsOperandNotInRangeZeroToNine(int operand)
+        {
+            return operand < 0 || operand > 9;
+        }
+
+        private string GetOperandWord(int operand)
+        {
+            return operands[operand];
+        }
+
+        public string GetOperator()
+        {
+            switch (operatorValue)
+            {
+                case 1:
+                    return "+";
+
+                case 2:
+                    return "*";
+
+                case 3:
+                    return "-";
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
